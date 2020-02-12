ef2809a945815d024338ead681bda75f348f155f
fiscal <fiscalguadalupe422@gmail.com>
2020-02-12 02:21:06 -0600

    primera prueba

---
+++ index.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilo.css">
+
+    <script type="text/javascript" src="js/JaS.js"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <!-- nav-->
+            <div class="col-12 fixed-top d-none d-md-block .bg-transparent" id="menu">
+                <h2 class="text-center">Pagina de prueba</h2>
+                <ul class=" estilonav nav justify-content-center">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Active</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+            <hr>
+            <!-- carrusel
+            <div class="col" id="carrusel">
+                <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active" data-interval="10000">
+                            
+                            <img src="imag/fondonav.jpg" class="d-block w-100 op" alt="Responsive image" class="img-fluid">
+                        </div>
+                        <div class="carousel-item" data-interval="2000">
+                            <img src="imag/fondonav.jpg" class="d-block w-100 op" alt="..." class="img-fluid">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="imag/fondonav.jpg" class="d-block w-100 op" alt="..." class="img-fluid">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+
+            </div>-->
+            <div class="col" id="carrusel">
+                <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="carousel-caption d-none d-md-block justify-content-between">
+                                <h5 class="h2">First slide label</h5>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                <button type="button" class="btn btn-outline-warning text-white">Warning</button>
+                            </div>
+                            <img src="imag/fondonav2.jpg" class="d-block w-100 op" alt="..." class="img-fluid">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="imag/fondonav2.jpg" class="d-block w-100 op" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="imag/fondonav2.jpg" class="d-block w-100 op" alt="...">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <!-- presentacion -->
+        <div class="row justify-content-center mt-5 ">
+            <div class="col">
+                <div class="card w-100 border-0">
+                    <div class="card-body text-center">
+                        <h5 class="card-title mt-5">Card title</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem veniam fugit porro voluptas doloremque commodi ab, cum quas ratione voluptate possimus rerum beatae quam voluptates maxime et amet accusamus ducimus?.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--cards img-->
+        <div class="row mt-5 d-flex justify-content-around">
+            <div class="col text-center">
+
+                <div class="card mb-3 p-5 border-0">
+                    <img src="imag/card1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col text-center">
+                <div class="card mb-3 p-5 border-0">
+                    <img src="imag/card2.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col text-center">
+                <div class="card mb-3 p-5 border-0">
+                    <img src="imag/card3.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <!-- our -->
+        <div class="row">
+            <div class="col">
+                <h2 class="text-center">trabajos</h2>
+                <ul class="estilotrabajo nav nav-justified">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Active</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- work -->
+        <div class="row mt-5 justify-content-center ml-4 mr-4">
+            <!-- card animada -->
+            <div class="col position-relative">
+                <img src="imag/work2.jpg" class="card-img img-fluid imagencard" alt="Responsive image">
+                <div class="mr-5 parrafo text-center position-absolute">
+                    <h1 class="text-left text-white">Galaxia</h1>
+                    <p class="text-white ml-3">ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card bg-dark text-white">
+                    <img src="imag/work1.jpeg" class="card-img img-fluid imagencard" alt="...">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">Last updated 3 mins ago</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card bg-dark text-white">
+                    <img src="imag/work3.jpg" class="card-img img-fluid imagencard" alt="...">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">Last updated 3 mins ago</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--footer-->
+        <footer class="row text-center d-flex justify-content-center mt-5">
+            <div class="col-md-2 mb-3">
+                <h6 class="text-uppercase">
+                    <a href="#!"> AAAAAAA</a>
+                </h6>
+            </div>
+            <div class="col-md-2 mb-3">
+                <h6 class="text-uppercase">
+                    <a href="#!"> BBBBB</a>
+                </h6>
+            </div>
+            <div class="col-md-2 mb-3">
+                <h6 class="text-uppercase">
+                    <a href="#!"> AAAAAAA</a>
+                </h6>
+            </div>
+            <div class="col-md-2 mb-3">
+                <h6 class="text-uppercase">
+                    <a href="#!"> AAAAAAA</a>
+                </h6>
+            </div>
+            <div class="col-md-2 mb-3">
+                <h6 class="text-uppercase">
+                    <a href="#!"> AAAAAAA</a>
+                </h6>
+            </div>
+            <hr class="w-100 clearfix d-md-none">
+
+            <div class="row d-flex text-center justify-content-center  mb-md-0 mb-4">
+                <div class="col-md-8 col-12 mt-5">
+                    <p style="line-height: 1.7rem">Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                        accusantium doloremque laudantium, totam rem
+                        aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+                        explicabo.
+                        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur.</p>
+                </div>
+            </div>
+            <div class="row pb-3">
+                <div class="col-md-12">
+                    <img src="" alt="">
+                    <label for="">Facebook</label>
+                </div>
+                <div class="col-md-12">
+                    <img src="" alt="">
+                    <label for="">Twitter</label>
+                </div>
+            </div>
+
+        </footer>
+
+
+
+
+    </div>
+    <script src="js/jquery-3.0.0.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <script>
+        $(window).scroll(function() {
+            if ($("#menu").offset().top > 56) {
+                $("#menu").addClass("azul");
+
+                // $("#menu").css( 'background-color', 'white', 'color');
+            } else {
+                $("#menu").removeClass("azul");
+            }
+
+
+        });
+    </script>
+</body>
+
+</html>
---
+++ js/JaS.js
@@ -0,0 +1,9 @@
+
+$(window).scroll(function() {
+    if ($("#menu").offset().top > 56) {
+        $("#menu").addClass("bg-dark");
+    } else {
+        $("#menu").removeClass("bg-dark");
+    }
+  });
+  

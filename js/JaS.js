b39a9253063d38836cfbc87c02bd45832c2ae367
fiscal <fiscalguadalupe422@gmail.com>
2020-07-01 22:09:22 -0500

    Se agrego imagenes del tema que mas me gusta

--- js/JaS.js
+++ js/JaS.js
@@ -1,67 +1,40 @@
-$(window).scroll(function () {
-    if ($("#menu").offset().top > 56) {
-        $("#menu").addClass("azul");
+var contador = 1;
 
-    } else{
-        $("#menu").removeClass("azul");
-    }
-
-});
-
-
-
-var contador=1;
-function navbar(){
+function navbar() {
     /*efecto navbar*/
-    $(window).scroll(function () {
-        if ($("#menu").offset().top > 56 ) {
+    $(window).scroll(function() {
+        if ($("#menu").offset().top > 56) {
             $("#menu").addClass("azul");
-            
-        }     
+
+        }
     });
 }
 
 
-window.onload = function () {
+window.onload = function() {
     document.getElementById('nav-button').addEventListener("click", clic);
-    contador=1;
+    contador = 1;
 }
 
 
 function clic() {
-    if (contador==1){
+    if (contador == 1) {
         $("#menu").addClass("azul");
-        contador=0;  
-    }else{
-        contador=1;
+        contador = 0;
+    } else {
+        contador = 1;
         navbar();
         $("#menu").removeClass("azul");
     }
 
-   
-}
-
-/*/efecto navbar
-$(window).scroll(function(event) {
-   // var scrollLeft = $(window).scrollLeft();
-   let animacion = document.getElementById('animado');
-   let tamPantalla = window.innerHeight / 2;
-   let posicionOnj1 = animacion.getBoundingClientRect().top;
-    var scrollTop = $(window).scrollTop();
-    console.log("posicion "+posicionOnj1);
-    console.log("tamPan "+tamPantalla);
-    console.log("Vertical "+scrollTop);
-
-
-
-
-    function main () {
-    $(window).scroll(function () {
-        if ($("#menu").offset().top > 56 && document.getElementById('nav-button').addEventListener("click", clic)) {
-            $("#menu").addClass("azul");
-           
-        }    
-    });
 
 }
-})*/
+
+window.onscroll = function() { myFunction() };
+
+function myFunction() {
+    if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+        //document.getElementById("myImg").className = "slideUp";
+        console.log("A");
+    }
+}
